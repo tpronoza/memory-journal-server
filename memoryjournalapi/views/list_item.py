de8bc8a99327dbc6851dda2b8d0115d32171327c
Tatiana Pronoza <tpronoza1@gmail.com>
2023-03-23 11:53:40 -0500

    cleanup

--- memoryjournalapi/views/list_item.py
+++ memoryjournalapi/views/list_item.py
@@ -8,7 +8,7 @@ from memoryjournalapi.serializers import ListSerializer, ListItemSerializer
 class ListItemView(ViewSet):
 
     def retrieve(self, request, pk):
-        """Handle GET request for single bandana_collection"""
+        """Handle GET request for single list_item"""
         try:
             list_item = List_Item.objects.get(pk=pk)
             serializer = ListItemSerializer(list_item)
@@ -59,44 +59,3 @@ class ListItemSerializer(serializers.ModelSerializer):
         model = List_Item
         fields = ('id', 'list', 'item')
         depth = 2
-
-# class ListItemView(ViewSet):
-
-#     def list(self, request):
-#         item_id = request.query_params.get('item_id')
-#         list_items = List_Item.objects.all()
-#         list_id = request.query_params.get('list_id')
-#         # list_user = request.query_params.get('user', None)
-#         # if list_user is not None:
-#         #     list_items.filter(user=list_user)
-            
-#         id_query = request.query_params.get('id', None)
-#         if id_query is not None:
-#             items = items.filter(list_items__item_id=item_id)
-#             lists = lists.filter(list_items__list_id=list_id)
-        
-#         serializer = ListItemSerializer(list_items, many=True)
-#         return Response(serializer.data)
-
-#     def create(self, request):
-#         list = List.objects.get(pk=request.data['list'])
-#         item = Item.objects.get(pk=request.data['item'])
-
-#         list_item = List_Item.objects.create(
-#             list=list,
-#             item = item
-#         )
-#         serializer = ListItemSerializer(list_item)
-#         return Response(serializer.data)
-
-#     def destroy(self, request, pk):
-#         list_item = List_Item.objects.get(pk=pk)
-#         list_item.delete()
-#         return Response(None, status=status.HTTP_204_NO_CONTENT)
-
-# class ListItemSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = List_Item
-#         fields = ('id', 'list', 'item')
-#         depth = 1
